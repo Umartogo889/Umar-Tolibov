2253095b2c66f98cd7bc0df099846b27661ac05f
Umar togo <you@example.com>
2024-02-24 13:31:31 +0500

    add aftor img

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const openBtn = document.querySelector(".burger")
 const closeBtn = document.querySelector(".close__btn")
 const ulList = document.querySelector(".nav__list")
+const listItem = document.querySelectorAll(".nav__item")
+
+listItem.forEach(item => {
+    item.addEventListener("click", () => {
+        ulList.classList.remove("nav__toggle")
+    })
+})
 
 openBtn.addEventListener("click", () => {
 
